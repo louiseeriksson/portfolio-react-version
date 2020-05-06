cfc6796d34d051b376adf65a0d2950a9923948cc
Louise Eriksson <louise@Louises-Air.lan>
2020-05-06 09:31:18 +0200

    fixed the src to all the images

--- src/components/More.js
+++ src/components/More.js
@@ -5,23 +5,23 @@ export const More = () => {
   return (
     <section class="portfolio-section for-more">
       <h3 class="bold-color">
-        FOR MORE
+        MORE
      </h3>
 
       <div class="for-more-icons">
         <Icon
           link='https://www.linkedin.com/in/louise-eriksson/'
-          src=''
+          src='assets/linkedin-color.svg'
           alt='LinkedIn logo'
           text='LINKEDIN' />
         <Icon
           link='https://github.com/louiseeriksson'
-          src=''
+          src='assets/github-color.svg'
           alt='GitHub logo'
           text='GITHUB' />
         <Icon
           link='https://stackoverflow.com/users/story/12700892?newreg=c03c8ca18af04a4582392b794da8b34a&_=1'
-          src=''
+          src='assets/stack-color.svg'
           alt='StackOverflow logo'
           text='STACKOVERFLOW' />
       </div>
